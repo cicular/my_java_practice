fb5ba72c134b56a3bdde42b50ba93818165dd49b
cicular <circularconstant2015@gmail.com>
2025-12-07 20:45:25 +0900

    文字の大小比較

---
+++ src/practice/experiment/CharCode.java
@@ -0,0 +1,14 @@
+package practice.experiment;
+
+public class CharCode {
+
+    public static void main(String[] args) {
+        
+        char chara = 'H';
+
+        if ( chara > 'A'){
+            // ASCIIコードで72が出力される。
+            System.out.println((int)chara);
+        }
+    }
+}
